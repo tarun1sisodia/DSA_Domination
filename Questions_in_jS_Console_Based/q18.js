07de751ac600521ef82881e964f2c3eab7bdeae0
= <coccoder999@gmail.com>
2025-07-18 17:45:36 +0530

    Refactor greatest of three numbers program and add launch configuration for debugging

--- Questions_in_jS_Console_Based/q18.js
+++ Questions_in_jS_Console_Based/q18.js
@@ -1,20 +1,23 @@
 //Accept three numbers and print the greatest among them.
 
-// let a = parseFloat(prompt('A:'));
-// let b = parseFloat(prompt('B:'));
-// let c = parseFloat(prompt('C:'));
+let a = parseFloat(prompt('A:'));
+let b = parseFloat(prompt('B:'));
+let c = parseFloat(prompt('C:'));
 
-let a = 1;
-let b = 3;
-let c = 5;
+// let a = 16;
+// let b = 13;
+// let c = 56;
 (() => {
     if (a > b && b > c) {
         alert('A', a, b, c,);
+        // console.log('A', a, b, c,);
     }
-    else if (b > c && a > c) {
+    else if (b > c && c > a) {
+        // console.log('B', a, b, c,);
         alert('B', a, b, c,);
     }
     else {
+        // console.log('C', a, b, c,);
         alert('C', a, b, c,);
 
     }
