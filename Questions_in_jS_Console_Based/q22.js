3c47bba082a345017efd1f9ef7dbc91e42a58435
= <coccoder999@gmail.com>
2025-07-18 17:55:46 +0530

    Add JavaScript program to check if a letter is a consonant or a vowel

--- Questions_in_jS_Console_Based/q22.js
+++ Questions_in_jS_Console_Based/q22.js
@@ -1,2 +1,14 @@
 //Accept an English alphabet from user and check if it is a consonant or a vowel.
 
+let englishLetter = prompt("Enter the Only 1 Letter.\n otherwise you will get consonant.").toLowerCase();
+
+
+
+((ch) => {
+    if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u') {
+        alert('Vowel..');
+    }
+    else {
+        alert("Consonant..")
+    }
+})(englishLetter);
