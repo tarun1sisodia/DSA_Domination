683df6598a31889c577c250a37224fe592279ac3
= <coccoder999@gmail.com>
2025-07-28 14:46:08 +0530

    my own logic and pure real code.

--- Questions_in_jS_Console_Based/q4.js
+++ Questions_in_jS_Console_Based/q4.js
@@ -7,8 +7,13 @@ r1.question('Enter your name:\t', (a) => {
     r1.question('Enter Your age:\t', (b) => {
         const name = a;
         const age = parseInt(b);
-        console.log(`Hello ${name}, You are ${age} years old.`);
-
+        callme(name, age);
         r1.close();
     })
 });
+
+function callme(name, age) {
+    return (() => {
+        console.log(`Hello ${name}, You are ${age} years old.`);
+    })(name, age);
+}
--- Questions_in_jS_Console_Based/q7.js
+++ Questions_in_jS_Console_Based/q7.js
@@ -6,14 +6,22 @@ const sc = readline.createInterface({ input: process.stdin, output: process.stdo
 
 sc.question('Enter The Length of Rectangle:', (len) => {
     sc.question('Enter the Breadth of Rectangle:', (bre) => {
-        const l = parseInt(len);
-        const b = parseInt(bre);
-
-        console.log(`Area of Rectange: ${l * b}`);
-        console.log(`Perimeter of Rectange: ${2 * (l * b)}`);
+        let l = +len;
+        let b = +bre;
+        triangle(l, b)
         sc.close()
-        
+
     });
 });
 
+function triangle(l, b) {
+    user?.area(l, b); //Here i have created an object that is performing directly operation to number.
+    user?.perimeter(l, b);
+    //Yahan humne object bana h aur usme ek humne optional chaining kri h  jisse if i did not get anything i will get undefined but not any error.
 
+}
+
+let user = {
+    area: ((l, b) => { console.log(`Area of Rectange: ${l * b}`); }),
+    perimeter: ((l, b) => { console.log(`Perimeter of Rectange: ${2 * (l * b)}`); })
+}
