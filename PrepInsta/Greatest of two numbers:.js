a8dfcdcb4f9d8cc2c4798677421c8a92b739163e
= <coccoder999@gmail.com>
2025-08-03 22:25:18 +0530

    greatest in 2

---
+++ PrepInsta/Greatest of two numbers:.js
@@ -0,0 +1,14 @@
+greatest(4, 2);
+
+function greatest(a, b) {
+    if (a > b) {
+        console.log(`A is greater`);
+
+    }
+    else if (a === b) {
+        console.log(`Both are Equal`);
+    }
+    else
+        console.log(`B is Greater`);
+
+}
