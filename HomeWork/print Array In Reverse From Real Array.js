f42f9507060d4f51fc2ba99e1707089e94ddf7e1
= <coccoder999@gmail.com>
2025-08-05 19:43:01 +0530

    print array in another array with reverse data

---
+++ HomeWork/print Array In Reverse From Real Array.js
@@ -0,0 +1,18 @@
+let arr = [10, 20, 30, 40];
+
+let n = arr.length;
+
+let newArray = new Array(n);
+
+let j = 0;
+
+for (let i = n - 1; i >= 0; i--) {
+    
+    newArray[j] = arr[i];
+    j++;
+}
+
+console.log(`Real: ${arr}`);
+console.log(`Copy : ${newArray}`);
+
+
