de15531a03e109b0a0a0ac06b7149d5a759c8657
= <coccoder999@gmail.com>
2025-08-05 19:42:44 +0530

    make a right shift with k elements

--- Questions_in_jS_Console_Based/q60.js
+++ Questions_in_jS_Console_Based/q60.js
@@ -1,39 +1,72 @@
 //Array left rotation by K elements
+function Rotation() {
+    let arr = [10, 20, 30, 40, 50];
 
-let arr = [10, 20, 30, 40, 50];
+    let k = 5;
 
-let k = 5;
+    let n = arr.length - 1;
 
-let n = arr.length - 1;
+    k = k % n;
 
-k = k % n;
+    for (let i = 0; i < k; i++) {
+        let temp = arr[0];
 
-for (let i = 0; i < k; i++) {
-    let temp = arr[0];
-
-    for (let j = 0; j < n; j++) {
-        arr[j] = arr[j + 1];
+        for (let j = 0; j < n; j++) {
+            arr[j] = arr[j + 1];
+        }
+        arr[n] = temp;
     }
-    arr[n] = temp;
-}
-// console.log(arr);
-
-
-
-let arr1 = [10, 20, 30, 40];
-let k1 = 2;
-let n1 = arr1.length;
-k1 = k1 % n1;
-console.log(arr1);
-
-for (let j = 0; j <= k; j++) {
-    temp = arr1[0];
-
-    for (let i = 0; i < n1 - 1; i++) {
-        arr1[i] = arr1[i + 1];
-
-    }
-    arr1[n1 - 1] = temp;
+    console.log(arr);
 }
 
-console.log(arr1);
+function leftShiftRotation() {
+    let arr1 = [10, 20, 30, 40];
+    let k1 = 2;
+    let n1 = arr1.length - 1;
+    k1 = k1 % n1;
+    console.log(arr1);
+
+    for (let j = 0; j <= k; j++) {
+        temp = arr1[0];
+
+        for (let i = 0; i < n1; i++) {
+            arr1[i] = arr1[i + 1];
+
+        }
+        arr1[n1] = temp;
+    }
+
+    console.log(arr1);
+}
+//Right Shift by 1.
+
+function rightShiftRotation() {
+    let arr = [10, 20, 30, 40];
+    let n = arr.length - 1;
+    temp = arr[n]
+    for (let i = n; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = temp;
+    console.log(arr);
+}
+// rightShiftRotation();
+
+
+
+function KrightShiftRotation(k) {
+    let arr = [10, 20, 30, 40];
+    let n = arr.length - 1;
+    k = Math.floor(k % n);
+
+    for (let j = 0; j <= k; j++) {
+        temp = arr[n];
+        for (let i = n; i > 0; i--) {
+            arr[i] = arr[i - 1]
+        }
+        arr[0] = temp;
+    }
+    console.log(arr);
+
+}
+KrightShiftRotation(3);
