17343fae5e034f417848ece3bcf18bf0504c020a
= <coccoder999@gmail.com>
2025-08-06 22:33:39 +0530

    constant time in Time Complexity

---
+++ Topics/O(1).js
@@ -0,0 +1,13 @@
+let students = ['Tarun', 'Rohit', 'HarshVardhan', 'Sanjana'];
+
+console.log(students[2]); // mtlb ye O(1) kyunki hum direct ek operation m pura khtm kr rhe h .
+//-----------------------------------------------------------------------//
+
+let n = 5;
+
+console.log(n * (n + 1) / 2);
+
+
+
+
+
