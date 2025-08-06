662ece46d6605717009cbbe32b60e20b9aa3f745
= <coccoder999@gmail.com>
2025-08-07 02:20:04 +0530

    update the q9 of linear

--- Complexity/Linear_Time/q9.js
+++ Complexity/Linear_Time/q9.js
@@ -1,7 +1,7 @@
 let list = ["Tarun", 21, "Github", "DSA"];
 let newList = [];
 
-for (const copy in list) {
-    newList[copy] = list[copy]
+for (const ele of list) {
+    newList.push(ele)
 }
 console.log(newList);
