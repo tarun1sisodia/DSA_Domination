2f67dfd2230291cc323fa9aeca5ec7efc007bd10
= <coccoder999@gmail.com>
2025-08-09 17:49:10 +0530

    Copy Array into another with BIG O of 1 Time Complexity.

--- Complexity/Linear_Time/q9.js
+++ Complexity/Linear_Time/q9.js
@@ -1,7 +1,9 @@
 let list = ["Tarun", 21, "Github", "DSA"];
-let newList = [];
+// This is very basic and easy and fast and also it maintains O(1) TC.
+let newList = [...list];
 
-for (const ele of list) {
-    newList.push(ele)
-}
+// for (const ele of list) {
+//     newList.push(ele)
+// }
+
 console.log(newList);
