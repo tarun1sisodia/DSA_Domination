f6671a64c4fff9bb470469e8a6726689c2940aab
tarun1sisodia <coccoder999@gmail.com>
2025-08-17 20:55:07 +0530

    Add frequency function to count character occurrences in a string

---
+++ Questions_in_jS_Console_Based/q111.js
@@ -0,0 +1,20 @@
+//Accept a string and print the frequency of each character present in the string
+
+function frequency(str) {
+    /*  let arr = [];
+     for (let i = 0; i < 128; i++) {
+         arr[i] = 0;
+     } */
+    let arr = new Array(128).fill(0);
+    // console.log(arr);
+    for (let i = 0; i < str.length; i++) {
+        let ch = str.charCodeAt(i);
+        arr[ch] += 1;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            console.log(`${String.fromCharCode(i)} -> ${arr[i]}`);
+        }
+    }
+}
+frequency("TarunSisodiaThakur")
