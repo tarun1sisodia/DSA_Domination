1f5ffc675a38135090991df456e82eb5b8605f10
tarun1sisodia <coccoder999@gmail.com>
2025-08-31 14:03:19 +0530

    more standard and stable code

--- CodeWithRohit/countVowelConsonant.js
+++ CodeWithRohit/countVowelConsonant.js
@@ -1,19 +1,41 @@
 function count(str) {
+
+    if (typeof str === "string") return countLetters(str);
+    else if (typeof str === "number") countNumber(str);
+    else return {
+        error: "Only Characters Accepts"
+    };
+
+}
+let nam = "Rohit Singh Bisht";
+console.log(count(nam));
+
+function countLetters(str) {
     let vowel = 0;
     let consonant = 0;
-    for (let i = 0; i < str.length; i++) {
-        let char = str.charAt(i).toLowerCase();
-        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
-            vowel++;
-        }
-        else if (char === " ") {
-            continue;
-        }
+    let str1 = str.toLowerCase();
+
+    for (let i = 0; i < str1.length; i++) {
+        let char = str1.charAt(i)
+
+        if ("aeiou".includes(char)) vowel++;
+        else if (char === " ") continue;
         else consonant++;
 
     }
-    console.log(`Vowels: ${vowel} & Consonant: ${consonant} Real String: ${str}`);
-
+    return { vowel, consonant, str1 };
 }
-let nam = "VowelConsonant";
-count(nam);
+function countNumber(str) {
+    if (str >= 0 && str <= Infinity) {
+        let even = 0;
+        let odd = 0;
+        for (let i = 0; i < str; i++) {
+            if (i % 2 === 0) {
+                even++;
+            }
+            else odd++;
+        }
+        // console.log(`Even: ${even} Odd:${odd}`);
+        return { even, odd };
+    }
+}
